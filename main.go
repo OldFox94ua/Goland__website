098f6941f0a833d6b11df7a0acfc7300eb2f566f
Vadim <mitai3627@gmail.com>
2021-12-06 22:29:11 +0200

    add Class and metod for info

--- main.go
+++ main.go
@@ -5,8 +5,21 @@ import (
 	"net/http"
 )
 
+type User struct {
+	name                  string
+	age                   uint16
+	money                 int16
+	avg_grades, happiness float64
+}
+
+func (u User) getAllInfo() string {
+	return fmt.Sprintf("User name is: %s. He is %d and"+
+		"he has money: %d", u.name, u.age, u.money)
+}
+
 func home_page(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Go easy!")
+	bob := User{"Bob", 25, -50, 4.2, 0.8}
+	fmt.Fprintf(w, bob.getAllInfo())
 }
 
 func contacts_page(w http.ResponseWriter, r *http.Request) {
@@ -20,5 +33,7 @@ func handleRequest() {
 }
 
 func main() {
+	//var bob User = ...
+
 	handleRequest()
 }
