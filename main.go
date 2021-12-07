39b6ea55b3fc3077c5d7cd6b576b2d814c5a0ce0
Vadim <mitai3627@gmail.com>
2021-12-07 22:11:41 +0200

    feat: create first templates

--- main.go
+++ main.go
@@ -2,28 +2,32 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
 type User struct {
-	name                  string
-	age                   uint16
-	money                 int16
-	avg_grades, happiness float64
+	Name                  string
+	Age                   uint16
+	Money                 int16
+	Avg_grades, Happiness float64
+	Hobbies               []string
 }
 
 func (u User) getAllInfo() string {
 	return fmt.Sprintf("User name is: %s. He is %d and"+
-		"he has money: %d", u.name, u.age, u.money)
+		"he has money: %d", u.Name, u.Age, u.Money)
 }
 
 func (u *User) setNewName(newName string) {
-	u.name = newName
+	u.Name = newName
 }
 
 func home_page(w http.ResponseWriter, r *http.Request) {
-	// bob := User{"Bob", 25, -50, 4.2, 0.8}
-	fmt.Fprintf(w, "<b>Main Text</b>")
+	bob := User{"Bobs", 25, -50, 4.2, 0.8, []string{"Footboll", "Skate", "Dance"}}
+	// fmt.Fprintf(w, "<b>Main Text</b>")
+	tmpl, _ := template.ParseFiles("templates/home_page.html")
+	tmpl.Execute(w, bob)
 }
 
 func contacts_page(w http.ResponseWriter, r *http.Request) {
